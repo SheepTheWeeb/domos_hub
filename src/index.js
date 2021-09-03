57592fa759336a700db01f1fe634c4b4da94a19f
jerraloid <jerryblaauw@hotmail.com>
2021-09-04 01:46:41 +0200

    setup nodemon and router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-const express = require('express')
-const app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+import dotenv from 'dotenv';
+import express from 'express';
+import Router from './router.js';
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(Router);
+
+const PORT = process.env.SERVER_PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}...`);
+});
